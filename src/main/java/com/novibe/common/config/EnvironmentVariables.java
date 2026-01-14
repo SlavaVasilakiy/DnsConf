4f8d7fa8283f5fc260f885fa71e96c35b50f8574
Vyacheslav Vasilakii <slk55slk@gmail.com>
2026-01-14 22:09:10 +0300

    Rename constants for clarity in EnvironmentVariables

--- src/main/java/com/novibe/common/config/EnvironmentVariables.java
+++ src/main/java/com/novibe/common/config/EnvironmentVariables.java
@@ -6,9 +6,9 @@ public class EnvironmentVariables {
 
     public static final String DNS = Objects.requireNonNull(System.getenv("DNS"));
 
-    public static final String NEXTDNS_PROFILE_ID = Objects.requireNonNull(System.getenv("CLIENT_ID"));
+    public static final String CLIENT_ID = Objects.requireNonNull(System.getenv("CLIENT_ID"));
 
-    public static final String NEXTDNS_API_KEY = Objects.requireNonNull(System.getenv("AUTH_SECRET"));
+    public static final String AUTH_SECRET = Objects.requireNonNull(System.getenv("AUTH_SECRET"));
 
     public static final String BLOCK = System.getenv("BLOCK");
 
